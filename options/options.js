b514388cda76fc7e45808b0a31a77984e6c4e744
Crowdbotics <team@crowdbotics.com>
2024-05-14 21:24:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-forest-47935",
+  url: "https://bitter-forest-47935.botics.co",
+  api: "https://bitter-forest-47935.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
